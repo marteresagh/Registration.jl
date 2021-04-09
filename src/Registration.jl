075da0d0080e0d9ab27128eadd2b70509c92c075
marteresagh <marteresa28@gmail.com>
2021-04-09 12:57:52 +0200

    update

--- src/Registration.jl
+++ src/Registration.jl
@@ -2,20 +2,11 @@ __precompile__()
 
 module Registration
 
-    # using Common
-    # using FileManager
+    using Common
+    using FileManager
     using PyCall
 
-    const o3d = PyNULL()
-
-    function __init__()
-        copy!(o3d, pyimport_conda("open3D.geometry","open3D"))
-    end
-
-    function hello()
-        @show o3d.PointCloud()
-    end
-    # include("ICP.jl")
+    include("ICP.jl")
 
     export hello
 end # module
