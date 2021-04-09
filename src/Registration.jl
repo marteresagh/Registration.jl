b62b4a73cd8917d2656fbee13efd81237e1598ee
marteresagh <marteresa28@gmail.com>
2021-04-09 12:45:38 +0200

    prova

--- src/Registration.jl
+++ src/Registration.jl
@@ -6,7 +6,14 @@ module Registration
     using FileManager
     using PyCall
 
-    include("ICP.jl")
+    const o3d = PyNULL()
+
+    function __init__()
+        copy!(o3d, pyimport_conda("open3D"))
+    end
+
+    @show o3d.geometry.PointCloud()
+    # include("ICP.jl")
 
     export Common, FileManager
 end # module
